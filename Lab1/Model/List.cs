c678f9c004466f48eb57eef43082253abcb8f517
Karin <karintoft@hotmail.com>
2012-09-01 21:18:05 +0200

    test

---
+++ Lab1/Model/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lab1.Model;
+      
+namespace Lab1 {
+    public class List {       
+        public string ListUsers(List<User> list) {
+            var query = list.Select(u => u.UserName).Take(10);
+            string user = "\n\nList 10 users:\n";
+            foreach (string q in query) {
+                user += q + "\n";
+            }
+            return string.Format("{0}", user);
+        } 
+    }
+}
---
+++ Lab1/Model/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lab1.Model;
+using Lab1.Helpers;
+
+namespace Lab1 {
+    public class Logger {
+        private static List<string> LastStrings = new List<string>();
+        public void Log(string msg) {
+            if (LastStrings.Count < 10) {
+                LastStrings.Add(msg);
+            }
+            else {
+                LastStrings.RemoveAt(0);
+                LastStrings.Add(msg);
+            }
+        }
+        public override string ToString() {
+            string msg = "\n\nList of latest commands:\n";
+            foreach (string item in LastStrings) {
+                msg += item + "\n";
+            }
+            return string.Format("{0}", msg);
+        }
+    }
+}
